2f32987a98a9be616e6e8cf61b684fc7fe8947df
sherlysaf <sherly.safitri_ti22@nusaputra.ac.id>
2026-07-22 23:41:32 +0700

    Create Laptop.java

---
+++ Laptop.java
@@ -0,0 +1,11 @@
+package laptop;
+
+public interface Laptop {
+    int MAX_VOL = 100;
+    int MIN_VOL = 0;
+
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
